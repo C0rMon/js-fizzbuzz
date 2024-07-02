201f4a714e051bbb63ed1c890cdecc07d2e62f4b
C0rMon <Colm.Monahan@softwire.com>
2024-07-02 16:39:33 +0100

    Added user input for max number. added readlineSync package

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,24 +1,19 @@
 // This is our main function
+
+
+
 function fizzbuzz() {
-    console.log("Hello, World!");
 
-    // Put your code here...
+
+    
+    var readlineSync = require("readline-sync")
+    var end = readlineSync.question("What number would you like to FizzBuzz to?")
+
     i = 1
-    while (i<=100) {
-        // FizzBuzz checks
-        const fizz = (i%3 == 0)
-        const buzz = (i%5 == 0)
-        const bang = (i%7 == 0)
-        const bong = (i%11 == 0)
-        const fezz = (i%13 == 0)
-        const reverse = (i%17 == 0)
-
-        // Creates the output
-        var output="";
-        if (reverse){
-
-        }
-        output = outputConstructor(fizz,buzz,bang,bong,fezz)
+    while (i<=end) {
+        
+        // Creates the output        
+        var output = outputConstructor()
         
         //Checks we dont need to print the number
         if (output == ""){
@@ -34,24 +29,34 @@ function fizzbuzz() {
 }
 // Constructs the output string based on previous checks
 // Order of if statments allow it to meet the requirements of part 1 and 2
-function outputConstructor(fizz,buzz,bang,bong,fezz){
-    var output="";
+function outputConstructor(){
+    const fizz = (i%3 == 0)
+    const buzz = (i%5 == 0)
+    const bang = (i%7 == 0)
+    const bong = (i%11 == 0)
+    const fezz = (i%13 == 0)
+    const reverse = (i%17 == 0)
 
-        if (fizz){
-            output = output + "Fizz"
-        }
-        if (buzz){
-            output = output + "Buzz"
-        }
-        if (bang){
-            output = output + "Bang"
-        }
-        if (bong){
-            output = "bong"
-        }
-        if (fezz){
-            output = fezz_add(output)
-        }
+    var output=[]
+
+    if (fizz){
+        output.push("Fizz")
+    }
+    if (buzz){
+        output.push("Buzz")
+    }
+    if (bang){
+        output.push("Bang")
+    }
+    if (bong){
+        output = ["Bong"]
+    }
+    if (fezz){
+        output = fezz_add(output)
+    }
+    if (reverse){
+        output.reverse()
+    }
     return output
 }
 
@@ -59,19 +64,27 @@ function outputConstructor(fizz,buzz,bang,bong,fezz){
 
 //Find B in the string and puts Fezz behind it
 //Puts Fezz at the end if no B in string
-function fezz_add(string){
-    const len =string.length
-    var output = ""
+function fezz_add(list){
+    const len =list.length
+    var output = []
     var fezz = false;
+
+    //if list is empty return Fezz
+    if (len == 0){
+        return ["Fezz"]
+    }
+
+    //Put Fezz before B in list
     for (var i = 0; i < len; i++){
-        if (string[i] == "B"){
-            output = output + "Fezz"
+        if (list[i][0] == "B"){
+           output.push("Fezz")
             fezz = true
         }
-        output = output + string[i]
+       output.push(list[i])
     }
+    //add Fezz at end if no B
     if (!fezz){
-        return (output + "Fezz")
+        return (output.push("Fezz"))
     }
     return output
 }
