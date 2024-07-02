7607872648a421dab7c27e6f14f6cf259be7c7e7
C0rMon <Colm.Monahan@softwire.com>
2024-07-02 11:38:00 +0100

    Part 1 complete

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,26 @@ function fizzbuzz() {
     console.log("Hello, World!");
 
     // Put your code here...
+    i = 1
+    while (i<=100) {
+        fizz = (i%3 == 0)
+        buzz = (i%5 == 0)
+        output=""
+        if (fizz){
+            output = output + "Fizz"
+        }
+        if (buzz){
+            output = output + "Buzz"
+        }
+        
+        if (output == ""){
+            console.log(i)
+        } else {
+            console.log(output)
+        }
+        i = i + 1
 
+    }
 }
 
 // Now, we run the main function:
