1540b7e22a647b1aba6af51e504306eaefb81647
C0rMon <Colm.Monahan@softwire.com>
2024-07-02 13:39:42 +0100

    Added bang, bong and fezz, put constructor in new function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,24 +5,75 @@ function fizzbuzz() {
     // Put your code here...
     i = 1
     while (i<=100) {
-        fizz = (i%3 == 0)
-        buzz = (i%5 == 0)
-        output=""
+        // FizzBuzz checks
+        const fizz = (i%3 == 0)
+        const buzz = (i%5 == 0)
+        const bang = (i%7 == 0)
+        const bong = (i%11 == 0)
+        const fezz = (i%13 == 0)
+        const reverse = (i%17 == 0)
+
+        // Creates the output
+        var output="";
+        if (reverse){
+
+        }
+        output = outputConstructor(fizz,buzz,bang,bong,fezz)
+        
+        //Checks we dont need to print the number
+        if (output == ""){
+            console.log(i)
+        } else {
+            console.log(output)
+        }
+
+        // increment
+        i = i + 1
+
+    }
+}
+// Constructs the output string based on previous checks
+// Order of if statments allow it to meet the requirements of part 1 and 2
+function outputConstructor(fizz,buzz,bang,bong,fezz){
+    var output="";
+
         if (fizz){
             output = output + "Fizz"
         }
         if (buzz){
             output = output + "Buzz"
         }
-        
-        if (output == ""){
-            console.log(i)
-        } else {
-            console.log(output)
+        if (bang){
+            output = output + "Bang"
         }
-        i = i + 1
+        if (bong){
+            output = "bong"
+        }
+        if (fezz){
+            output = fezz_add(output)
+        }
+    return output
+}
 
+
+
+//Find B in the string and puts Fezz behind it
+//Puts Fezz at the end if no B in string
+function fezz_add(string){
+    const len =string.length
+    var output = ""
+    var fezz = false;
+    for (var i = 0; i < len; i++){
+        if (string[i] == "B"){
+            output = output + "Fezz"
+            fezz = true
+        }
+        output = output + string[i]
     }
+    if (!fezz){
+        return (output + "Fezz")
+    }
+    return output
 }
 
 // Now, we run the main function:
